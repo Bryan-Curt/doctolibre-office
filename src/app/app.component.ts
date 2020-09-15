0842e810a1177c77fa43fa12c2a2a6ca6d22c881
Valentin Delpy <soulhokib@gmail.com>
2020-09-15 16:06:13 +0200

    Création des services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ServiceService } from './service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'doctolibre-office';
+
+  constructor(private service: ServiceService) {
+    service.putAppointment('{\n' +
+      '    "resourceType": "Patient",\n' +
+      '    "active": true,\n' +
+      '    "name": [\n' +
+      '        {\n' +
+      '            "use": "official",\n' +
+      '            "family": "Cryan",\n' +
+      '            "given": [\n' +
+      '                "Burt",\n' +
+      '                "Albert"\n' +
+      '            ]\n' +
+      '        }\n' +
+      '    ]\n' +
+      '}').then(
+      patients => {
+        console.log(patients);
+      });
+  }
 }
