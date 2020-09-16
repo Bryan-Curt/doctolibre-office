0e2d8f08d0a6586ef4c27b6a28e58a7cae9aaf12
Valentin Delpy <soulhokib@gmail.com>
2020-09-16 11:37:09 +0200

    Changement de format de date

--- src/app/prise-rdv/prise-rdv.component.ts
+++ src/app/prise-rdv/prise-rdv.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import {ServiceService} from '../service.service';
 import { FormsModule }   from '@angular/forms';
-import {Data} from '../data'
-import { Reference } from '../data'
-import {Participants} from '../data'
+import {Data} from '../data';
+import { Reference } from '../data';
+import {Participants} from '../data';
 
 @Component({
   selector: 'app-prise-rdv',
@@ -13,7 +13,7 @@ import {Participants} from '../data'
 })
 export class PriseRdvComponent implements OnInit {
   practitioners = [];
-  horaireHeure = ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"];
+  horaireHeure = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
   data = new Data() ;
   pracRef = new Reference() ;
   patRef = new Reference();
@@ -40,23 +40,25 @@ export class PriseRdvComponent implements OnInit {
 
   onSubmit() {
     let med: any;
-    for(let p of this.practitioners){
-      if(p.name[0].family == this.infoReservation.value.medecin){
+    for (const p of this.practitioners){
+      if (p.name[0].family == this.infoReservation.value.medecin){
           med = p;
       }
     }
-    this.patRef.reference = 'Patient/5f5f8f733ef92800151f13aa'
+    const dateStart = new Date(this.infoReservation.value.jour + ' ' + this.infoReservation.value.heure + ':00');
+    const dateEnd = dateStart;
+    dateEnd.setHours(dateEnd.getHours() + 1);
+    console.log(dateEnd);
+    this.patRef.reference = 'Patient/5f5f8f733ef92800151f13aa';
     this.pracRef.reference = 'Practitioner/' + med.id;
     this.pat.actor = this.patRef;
     this.prac.actor = this.pracRef;
-    this.pat.status = 'accepted'
-    this.prac.status = "accepted"
-    this.data.status = "pending"
-    this.data.participant = [this.pat, this.prac]
-    this.infoReservation.value.heure = this.horaireHeure[this.horaireHeure.indexOf(this.infoReservation.value.heure)-1]
-    this.data.start = this.infoReservation.value.jour + "T" + this.infoReservation.value.heure + ":00Z"
-    this.infoReservation.value.heure = this.horaireHeure[this.horaireHeure.indexOf(this.infoReservation.value.heure)+1]
-    this.data.end = this.infoReservation.value.jour + "T" + this.infoReservation.value.heure + ":00Z"
+    this.pat.status = 'accepted';
+    this.prac.status = 'accepted';
+    this.data.status = 'pending';
+    this.data.participant = [this.pat, this.prac];
+    this.data.start = dateStart.toISOString();
+    this.data.end = dateEnd.toISOString();
     console.log(this.data);
 
     this.service.insertAppointment(this.data);
